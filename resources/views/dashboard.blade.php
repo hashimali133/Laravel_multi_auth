3e5d21b13277a04bf4ec5997e08a2741fa0c8671
Hashim Ali <hashimali133>
2025-04-22 02:54:06 +0500

    modified:   package-lock.json 	modified:   package.json 	modified:   resources/js/app.js 	modified:   resources/views/dashboard.blade.php 	new file:   resources/views/index

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,11 +1,13 @@
 <!doctype html>
-<html lang="en">
+<html lang="en-US">
 
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>Laravel 11 Multi Auth</title>
     <link rel="stylesheet" href="https://unpkg.com/bootstrap@5.3.2/dist/css/bootstrap.min.css">
+
+
 </head>
 
 <body class="bg-light">
@@ -53,11 +55,14 @@
             <div class="card-body">
                 You are logged in !!
             </div>
+
         </div>
     </div>
+     
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-kenU1KFdBIe4zVF0s0G1M5b4hcpxyD9F7jL+jjXkk+Q2h455rYXK/7HAuoJl+0I4" crossorigin="anonymous">
     </script>
+
 </body>
 
 </html>
